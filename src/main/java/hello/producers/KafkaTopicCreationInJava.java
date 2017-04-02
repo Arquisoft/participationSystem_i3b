e192c3272c5d2e160ab00a1fd4785df2df227d56
OriolInvernonLlaneza <93silf@gmail.com>
2017-04-02 15:09:00 +0200

    Class for creating Kafka topics done

---
+++ src/main/java/hello/producers/KafkaTopicCreationInJava.java
@@ -0,0 +1,42 @@
+package hello.producers;
+
+import java.util.Properties;
+
+import kafka.admin.AdminUtils;
+import kafka.utils.ZKStringSerializer$;
+import kafka.utils.ZkUtils;
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.ZkConnection;
+
+
+/**
+ * Created by Oriol on 02/04/2017.
+ */
+public class KafkaTopicCreationInJava {
+    public static void createTopic(String args) throws Exception {
+        ZkClient zkClient = null;
+        ZkUtils zkUtils;
+        try {
+            String zookeeperHosts = "192.168.20.1:2181";
+            int sessionTimeOutInMs = 15 * 1000; // 15 secs
+            int connectionTimeOutInMs = 10 * 1000; // 10 secs
+
+            zkClient = new ZkClient(zookeeperHosts, sessionTimeOutInMs, connectionTimeOutInMs, ZKStringSerializer$.MODULE$);
+            zkUtils = new ZkUtils(zkClient, new ZkConnection(zookeeperHosts), false);
+
+            String topicName = args;
+            int noOfPartitions = 2;
+            int noOfReplication = 3;
+            Properties topicConfiguration = new Properties();
+
+            AdminUtils.createTopic(zkUtils, topicName, noOfPartitions, noOfReplication, topicConfiguration);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (zkClient != null) {
+                zkClient.close();
+            }
+        }
+    }
+}
