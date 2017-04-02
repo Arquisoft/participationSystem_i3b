1c6580939adf16fc143504601f18fc67d893dc8b
OriolInvernonLlaneza <93silf@gmail.com>
2017-04-03 01:35:00 +0200

    Revert changes on proposal (null pointer)

--- src/main/java/hello/model/Proposal.java
+++ src/main/java/hello/model/Proposal.java
@@ -110,22 +110,48 @@ public class Proposal extends AbstractVotable {
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
 		Proposal proposal = (Proposal) o;
 
-		if (!id.equals(proposal.id)) return false;
-		if (!author.equals(proposal.author)) return false;
-		return title != null ? title.equals(proposal.title) : proposal.title == null;
+		if (isAccepted != proposal.isAccepted)
+			return false;
+		if (id != null ? !id.equals(proposal.id) : proposal.id != null)
+			return false;
+		if (author != null ? !author.equals(proposal.author)
+				: proposal.author != null)
+			return false;
+		if (comments != null ? !comments.equals(proposal.comments)
+				: proposal.comments != null)
+			return false;
+		if (category != null ? !category.equals(proposal.category)
+				: proposal.category != null)
+			return false;
+		if (title != null ? !title.equals(proposal.title)
+				: proposal.title != null)
+			return false;
+		if (content != null ? !content.equals(proposal.content)
+				: proposal.content != null)
+			return false;
+		return expirationDate != null ? expirationDate.equals(
+				proposal.expirationDate) : proposal.expirationDate == null;
 
 	}
 
 	@Override
 	public int hashCode() {
-		int result = id.hashCode();
-		result = 31 * result + author.hashCode();
+		int result = id != null ? id.hashCode() : 0;
+		result = 31 * result + (author != null ? author.hashCode() : 0);
+		result = 31 * result + (comments != null ? comments.hashCode() : 0);
+		result = 31 * result + (isAccepted ? 1 : 0);
+		result = 31 * result + (category != null ? category.hashCode() : 0);
 		result = 31 * result + (title != null ? title.hashCode() : 0);
+		result = 31 * result + (content != null ? content.hashCode() : 0);
+		result = 31 * result + (expirationDate != null ? expirationDate
+				.hashCode() : 0);
 		return result;
 	}
 
